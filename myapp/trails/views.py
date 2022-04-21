fb43bd6b04edc08dfef2fa0ec868cd6901bb949e
jurgenstevens <jurgen.stevens@yahoo.com>
2022-04-20 23:59:17 -0500

    Include create view in views.py.

--- myapp/trails/views.py
+++ myapp/trails/views.py
@@ -5,3 +5,22 @@ from myapp.models import Trail
 from myapp.trails.forms import TrailForm
 
 trails = Blueprint('trails', __name__)
+
+@trails.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_trail():
+    form = TrailForm()
+    if form.validate_on_submit():
+        trail = Trail(
+            trail_name=form.trail_name.data, 
+            distance=form.distance.data, 
+            review=form.review.data, 
+            comment=form.comment.data, 
+            trail_image=form.trail_image.data, 
+            user_id=current_user.id)
+        db.session.add(trail)
+        db.session.commit()
+        flash('Trail Created')
+        print('Trail was created')
+        return redirect(url_for('core.index'))
+    return render_template('create_trail.html', form=form)
